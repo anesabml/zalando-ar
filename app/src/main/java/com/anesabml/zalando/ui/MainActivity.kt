41687c557b7cf5a7653a4901a1f751a8f6a52541
Anes Abismail <anesabismail@gmail.com>
2020-08-23 14:39:27 +0100

    feat(app): add NavigationGraph

--- app/src/main/java/com/anesabml/zalando/ui/MainActivity.kt
+++ app/src/main/java/com/anesabml/zalando/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.anesabml.zalando
+package com.anesabml.zalando.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anesabml.zalando.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
